4e1607722cbae3499c2200809e3d6b20ed678853
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-25 19:47:21 -0400

    Update Codes.lua

--- Codes.lua
+++ Codes.lua
@@ -2,12 +2,4 @@
 -- Managed by the Discord bot.
 
 return {
-    ["HEY"] = {
-        Redeems = 3,
-        Duration = "3m"
-    },
-    ["O7RO-99TM-K0BT"] = {
-        Redeems = 1,
-        Duration = "1m"
-    },
 }
