0df0ad8bb21e46ca6de53424ff1c965a8ae730bf
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-27 17:27:29 -0400

    Sync Base44 generated codes

--- Codes.lua
+++ Codes.lua
@@ -2,7 +2,31 @@
 -- Managed by Discord bot.
 
 return {
+    ["0C55-A7SX-CGMN"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
+    ["2HJI-99TM-6C8V"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
+    ["O7RO-99TM-K0BT"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
+    ["SVP6-D9TM-85ME"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
+    ["TLAD-ZBTM-ROTE"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
     ["XXQR-GIVE-AWAY"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
+    ["YC3N-99TM-DR9M"] = {
         Redeems = 1,
         Duration = "1h"
     },
