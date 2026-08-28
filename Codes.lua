ece5a61972edfc85358373266a008dd49422ef2a
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-27 23:20:56 -0400

    Redeem code D1PV-DCTM-K4YY

--- Codes.lua
+++ Codes.lua
@@ -2,10 +2,6 @@
 -- Managed by Discord bot.
 
 return {
-    ["D1PV-DCTM-K4YY"] = {
-        Redeems = 1,
-        Duration = "1h"
-    },
     ["DKIR-DCTM-D44S"] = {
         Redeems = 1,
         Duration = "1h"
