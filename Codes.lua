faad1d442fb40143f90323cc75483e8a83475214
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-27 23:20:17 -0400

    Import Base44 generated codes

--- Codes.lua
+++ Codes.lua
@@ -2,6 +2,10 @@
 -- Managed by Discord bot.
 
 return {
+    ["D1PV-DCTM-K4YY"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
     ["DKIR-DCTM-D44S"] = {
         Redeems = 1,
         Duration = "1h"
