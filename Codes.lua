ee4e8fe0807f70a381baf51db50997b0571b0e2c
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-29 15:20:49 -0400

    Import Base44 generated codes

--- Codes.lua
+++ Codes.lua
@@ -6,6 +6,10 @@ return {
         Redeems = 1,
         Duration = "1h"
     },
+    ["A2EM-RETM-ZNJM"] = {
+        Redeems = 1,
+        Duration = "1h"
+    },
     ["DKIR-DCTM-D44S"] = {
         Redeems = 1,
         Duration = "1h"
